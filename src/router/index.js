4f9a933e40c9f3651336910e4c13229a63a40cd1
liuloudemao <2492194674@qq.com>
2019-02-19 20:16:18 +0800

    登录路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login.vue'
 
 Vue.use(Router)
 
@@ -8,6 +9,9 @@ export default new Router({
     {
       path: '/',
       name: 'home'
-    }
-  ]
+    }, {
+      name: 'login',
+      path: '/login',
+      component: Login
+    }]
 })   
