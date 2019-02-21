7a44ea4217b52453f0765bd4fe3c9ad5e903fe68
liuloudemao <2492194674@qq.com>
2019-02-21 10:23:12 +0800

    处理登录请求的res

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,8 +1,6 @@
 <template>
   <dir class="login-wrap">
-    <el-form 
-    class="login-form"
-    label-position="top" label-width="80px" :model="formdata">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
       <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
@@ -10,23 +8,64 @@
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="primary">登录</el-button>
     </el-form>
   </dir>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       formdata: {
-        username: '',
-        password: ''
+        username: "",
+        password: ""
       }
+    };
+  },
+  methods: {
+    // 发送登录请求
+    handleLogin() {
+      this.$http
+        .post(`login`, this.formdata)
+        .then(res => {
+          console.log(res);
+          const {
+            data: {
+              data,
+              meta: { msg, status }
+            }
+          } = res;
+          if (status === 200) {
+            console.log("success-----");
+          } else {
+            // 提示框
+            this.$message.error(msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
-}
+};
 </script>
 
 <style>
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background-color: #ffffff;
+  border-radius: 5px;
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
