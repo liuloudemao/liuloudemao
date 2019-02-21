35527db20755ef5ea73a3aff9c9bdea2e4a6f057
liuloudemao <2492194674@qq.com>
2019-02-21 11:59:01 +0800

    头部样式调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,22 @@
 
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
-      <el-main class="main" >Main</el-main>
+      <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
@@ -15,13 +27,22 @@ export default {};
 
 <style>
 .container {
-    height: 100%;
-    background-color: dimgray;
+  height: 100%;
+  background-color: #B3C0D1;
 }
 .aside {
-    background-color: steelblue;
+  background-color: steelblue;
 }
 .main {
-    background-color: violet;
+  background-color: violet;
+}
+.middle {
+    line-height: 20px;
+    text-align: center;
+    color: #ffffff;
+}
+.logout {
+    line-height: 60px;
+    text-decoration: none;
 }
 </style>
