8ba3634b6755b4e523a66d12469af85d80786853
liuloudemao <2492194674@qq.com>
2019-02-21 21:43:06 +0800

    进入首页的权限验证

--- src/components/home.vue
+++ src/components/home.vue
@@ -15,20 +15,129 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <el-menu
+          :unique-opened="true"
+          :router="true"
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <!-- 第一个 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 第二个 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 第三个 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 第四个 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 第五个 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  // 如果用户没登录 if(token) 改标识this.$router.push 显示Login.vue
+  // 如果登录了 if(tokrn) 继续渲染hovme.vue
+  // 代码位置：什么情况下？执行if else? 组件渲染完成 vue加载完组件后
+
+  // 页面加载之前
+  // beforeCreate() {
+
+  // },
+
+  beforeMount() {
+    if (!localStorage.getItem("token")) {
+      this.$router.push({
+        name: "login"
+      });
+      this.$message.warning("请先登录");
+    }
+  },
+  // 页面加载之后
+  mounted() {
+    console.log(11111);
+  }
+};
 </script>
 
 <style>
 .container {
   height: 100%;
-  background-color: #B3C0D1;
+  background-color: #b3c0d1;
 }
 .aside {
   background-color: steelblue;
@@ -37,12 +146,12 @@ export default {};
   background-color: violet;
 }
 .middle {
-    line-height: 20px;
-    text-align: center;
-    color: #ffffff;
+  line-height: 20px;
+  text-align: center;
+  color: #ffffff;
 }
 .logout {
-    line-height: 60px;
-    text-decoration: none;
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
